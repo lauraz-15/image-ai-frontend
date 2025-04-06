7b3025b4c0b4881efa6fcbbe45e85b3ac1e551ef
Laura <laura.zvirbule90@gmail.com>
2025-04-06 16:24:35 +0100

    remove stripe version specs

--- pages/api/checkout-sessions.ts
+++ pages/api/checkout-sessions.ts
@@ -2,9 +2,7 @@
 import Stripe from "stripe";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2022-11-15",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
