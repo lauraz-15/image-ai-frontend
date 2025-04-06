470c8f0833a8e6b509abfdca8c26a85384f458ef
Laura <laura.zvirbule90@gmail.com>
2025-04-06 18:24:16 +0100

    add error page

---
+++ app/error/page.tsx
@@ -0,0 +1,10 @@
+// app/error/page.tsx
+export default function ErrorPage() {
+    return (
+      <div>
+        <h1>An Error Occurred</h1>
+        <p>Sorry, something went wrong.</p>
+        
+      </div>
+    );
+  }
